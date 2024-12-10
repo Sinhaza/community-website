f15563f669f3e656ab1f618b2eae8a7f76a5fd38
Sinhaza <sinhazaruptura@gmail.com>
2024-12-10 17:34:19 +0100

    Changed background colour

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -91,33 +91,37 @@ function getRandomPosts(thread) {
         <!-- Groups end -->
 
         <!-- Hero -->
-        <div class="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8">
-            <div class="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pr-10">
-                <div class="text-left">
-                    <h2
-                        class="text-4xl font-extrabold leading-10 tracking-tight text-gray-800 sm:text-5xl sm:leading-none md:text-6xl">
-                        All lone on a
-                        <span class="font-bold text-blue-500">Friday night?</span>
-                    </h2>
-                    <p
-                        class="max-w-md mx-auto mt-3 font-bold text-base text-gray-500 sm:text-lg md:mt-5 md:text-4xl md:max-w-3xl">
-                        Not with us!
-                    </p>
-                    <p class="max-w-md mx-auto mt-3 text-base text-gray-800 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                        Re-experience your golden highschool days with us together in VRChat
-                    </p>
-                    <div class="mt-5 sm:flex md:mt-8">
-                        <div class="rounded-md shadow"><a href="https://discord.com/servers/vrchat-189511567539306508"
-                                class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
-                                Start here!
-                            </a></div>
+        <div class="w-full bg-gray-100">
+            <div class="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8">
+                <div class="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pr-10">
+                    <div class="text-left">
+                        <h2
+                            class="text-4xl font-extrabold leading-10 tracking-tight text-gray-800 sm:text-5xl sm:leading-none md:text-6xl">
+                            All lone on a
+                            <span class="font-bold text-blue-500">Friday night?</span>
+                        </h2>
+                        <p
+                            class="max-w-md mx-auto mt-3 font-bold text-base text-gray-500 sm:text-lg md:mt-5 md:text-4xl md:max-w-3xl">
+                            Not with us!
+                        </p>
+                        <p
+                            class="max-w-md mx-auto mt-3 text-base text-gray-800 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                            Re-experience your golden highschool days with us together in VRChat
+                        </p>
+                        <div class="mt-5 sm:flex md:mt-8">
+                            <div class="rounded-md shadow"><a
+                                    href="https://discord.com/servers/vrchat-189511567539306508"
+                                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
+                                    Start here!
+                                </a></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pl-10">
-                <div class="relative w-full p-3 rounded  md:p-8">
-                    <div class="rounded-lg bg-white text-black w-full">
-                        <img src="/images/group/1733731177738766.jpg" />
+                <div class="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pl-10">
+                    <div class="relative w-full p-3 rounded  md:p-8">
+                        <div class="rounded-lg bg-white text-black w-full">
+                            <img src="/images/group/1733731177738766.jpg" />
+                        </div>
                     </div>
                 </div>
             </div>
