c258b0ae4d9461faec3d8e3eeea509872acd93a1
Sinhaza <sinhazaruptura@gmail.com>
2025-01-13 11:50:21 +0100

    removes TOS

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -170,19 +170,6 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 
     <div class="pt-6 mt-12 border-t border-gray-800">
       <div class="text-center  sm:flex sm:justify-between sm:text-left">
-        <p class="text-sm space-x-1 text-gray-400">
-          <span class="block sm:inline">All rights reserved.</span>
-
-          <a class="inline-block text-teal-500 underline transition hover:text-teal-500/75" href="/">
-            Terms & Conditions
-          </a>
-
-          <span>&middot;</span>
-
-          <a class="inline-block text-teal-500 underline transition hover:text-teal-500/75" href="/">
-            Privacy Policy
-          </a>
-        </p>
 
         <p class="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
           &copy; 2024 /vrg/
